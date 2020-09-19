1b471028abc1fdf205d353eee47b0e4b32758ba8
vladislavgribkov <70588161+vladislavgribkov@users.noreply.github.com>
2020-09-19 15:06:33 +0300

    feat: comlited task simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position <= 0 || typeof position !== 'number' || (position >= this.getLength())) {
+      this.chain = [];
+      throw new Error();
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let resultChain = this.chain.map( elem => `( ${elem} )`);
+    this.chain = [];
+    return resultChain.join('~~');
   }
 };
 
