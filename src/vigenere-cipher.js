ea2be389c4afd02861c5516a936c2eae5ce39318
vladislavgribkov <vladislavgrbkovq@yandex.by>
2020-09-22 00:21:42 +0300

    feat: coplited task vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,103 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type){
+    this.type = type;
+  }
+  encrypt(message, key) {
+    if(message === undefined || key === undefined){
+        throw new Error('Error');
+    }
+    
+   message = message.toUpperCase();
+   key = key.toUpperCase();
+   let arr = key.split("");
+   let arrMessage = message.split("");
+    let outArray = []; 
+   if(message.length > key.length){
+       let i = 0;
+       let j = key.length;
+       while(j !== message.length){
+           arr.push(arr[i]);
+           i++;
+           j++;
+       }
+   }else{
+        let j = key.length;
+        while(j !== message.length){
+            arr.splice(arr.length-1,1);
+            j--;
+        }
+   }
+   let j = 0;
+   for(let i = 0; i < arrMessage.length; i++){
+       
+        if((64 < arrMessage[i].charCodeAt() && arrMessage[i].charCodeAt()<91 && arrMessage[j].charCodeAt() < 91) || arrMessage[i]===" "){
+
+            if(arrMessage[i]!==' '){
+                let char = (((arrMessage[i].charCodeAt() + arr[j].charCodeAt())%26) + 65);
+                outArray.push(String.fromCharCode(char));
+            }else{
+                --j;
+                outArray.push(" ");
+            }
+            
+        }else{
+            outArray.push(arrMessage[i]);
+        }
+        j++;
+   }
+   if(this.type === false){
+    outArray = outArray.reverse();
+    }
+ return outArray.join("");
+}    
+
+decrypt(message, key) {
+    if(message === undefined || key === undefined){
+        throw new Error('Error');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let arr = key.split("");
+    let arrMessage = message.split("");
+     let outArray = []; 
+    if(message.length > key.length){
+        let i = 0;
+        let j = key.length;
+        while(j !== message.length){
+            arr.push(arr[i]);
+            i++;
+            j++;
+        }
+    }else{
+         let j = key.length;
+         while(j !== message.length){
+             arr.splice(arr.length-1,1);
+             j--;
+         }
+    }
+    let j = 0;
+    for(let i = 0; i < arrMessage.length; i++){
+         if((64 < arrMessage[i].charCodeAt() && arrMessage[i].charCodeAt()<91 && arrMessage[j].charCodeAt() < 91) || arrMessage[i]===" "){
+            
+             if(arrMessage[i]!==' '){
+                 let char = ((((arrMessage[i].charCodeAt() - arr[j].charCodeAt())+26) % 26) + 65);
+                 outArray.push(String.fromCharCode(char));
+             }else{
+                 --j;
+                 outArray.push(" ");
+             }
+             
+         }else{
+             outArray.push(arrMessage[i]);
+         }
+         j++;
+    }
+    if(this.type === false){
+        outArray = outArray.reverse();
+    }
+    return outArray.join("");
   }
 }
 
