4447c1ea6fe2dfea16a3faabbe968308504c7c31
vladislavgribkov <vladislavgrbkovq@yandex.by>
2020-09-21 18:18:43 +0300

    feat: complited task recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,7 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-    
-  }
+  calculateDepth(arr, countArr = [], count = 1) {
+  countArr.push(count);
+      for(let i = 0; i<arr.length; i++){
+        if(Array.isArray(arr[i])){
+          this.calculateDepth(arr.flat(1), countArr, ++count);
+          break;
+        }
+      }
+    return Math.max(...countArr);
+  }
 };
